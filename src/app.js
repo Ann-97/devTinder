325836a32c1c6f591a7d3424f4f4c56074a3ab1a
Ann Maria Varghese <annmariavithayathil@gmail.com>
2026-03-26 12:41:36 +0530

    handling multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
-const express = require('express');
+const express = require ('express');
 const app = express();
-app.get("/user/:userId", (req, res) => {
-    console.log(req.params,"paramsss");   
- res.send({firstName:"Ann",lastName:"John"});
-});
+app.use("/user",
+    (req,res,next)=>{
+    console.log("1stttttt"); 
+ res.send("1st res")
+      next()  
+
+},
+(req,res,next)=>{
+    console.log("2ndddd");   
+ res.send("2nd res")
+ next()
+},
+(req,res,next)=>{
+    console.log("3rddd");   
+ res.send("3rd res")
+ next()
+},
+)
 
 app.listen(3000,()=>{
     console.log("server is successfully listening on port 3000");
