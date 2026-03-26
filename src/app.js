a1018cbd522ba0fea235d9f6179dac4ba22947d6
Ann Maria Varghese <annmariavithayathil@gmail.com>
2026-03-26 09:27:28 +0530

    exploring api's

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 // initialize the app and make it listen to a port.
 const app = express();
-
-app.use((req,res)=>{
-    res.send("helloooo ............")
-})
+// This will only handle GET requests to /user
+app.get("/user", (req, res) => {
+ res.send({firstName:"Ann",lastName:"John"});
+});
+app.post("/user", (req, res) => {
+ res.send("data saved successfully")
+});
+// This will handle all HTTP method requests to /test
+app.use("/hello", (req, res) => {
+ res.send("hello hello hello!!");
+});
+// app.use("/",(req,res)=>{
+//     res.send("helloooo ............")
+// })
 app.listen(3000,()=>{
     console.log("server is successfully listening on port 3000");
     
