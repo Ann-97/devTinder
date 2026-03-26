1cf2a0c59c57c36a91fd0ce19db2421a25eda469
Ann Maria Varghese <annmariavithayathil@gmail.com>
2026-03-26 10:31:18 +0530

    dynamic routes

--- src/app.js
+++ src/app.js
@@ -1,22 +1,11 @@
 const express = require('express');
-// initialize the app and make it listen to a port.
 const app = express();
-// This will only handle GET requests to /user
-app.get("/user", (req, res) => {
+app.get("/user/:userId", (req, res) => {
+    console.log(req.params,"paramsss");   
  res.send({firstName:"Ann",lastName:"John"});
 });
-app.post("/user", (req, res) => {
- res.send("data saved successfully")
-});
-// This will handle all HTTP method requests to /test
-app.use("/hello", (req, res) => {
- res.send("hello hello hello!!");
-});
-// app.use("/",(req,res)=>{
-//     res.send("helloooo ............")
-// })
+
 app.listen(3000,()=>{
     console.log("server is successfully listening on port 3000");
     
 });
-// we’ve created our first server 
