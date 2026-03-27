a45e39f81cc3e86be5039724c56b7d62a767a912
Ann Maria Varghese <annmariavithayathil@gmail.com>
2026-03-27 09:36:37 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -1,25 +1,18 @@
 const express = require ('express');
-const {adminAuth,userAuth} = require ('./middleware/auth');
 
 const app = express();
 
-app.use("/admin",adminAuth)
-// app.use("/user",userAuth)
-
-app.get("/admin/getAllData",(req,res)=>{
- res.send("All data sent")
-
+app.get("/getUserData",(req,res)=>{
+    throw new Error("sasdvbf")
+    res.send("userdata send") 
 })
-app.post("/user",(req,res,next)=>{
- res.send("user loggedin")
-      next()  
 
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("something went wrong")
+    }
 })
-app.use("/user",userAuth,(req,res,next)=>{
- res.send("user data sent")
-      next()  
 
-})
 
 app.listen(3000,()=>{
     console.log("server is successfully listening on port 3000");
