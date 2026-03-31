fd3cdcc8d7839b1ffe480857c84e18fc5e702294
Ann Maria Varghese <annmariavithayathil@gmail.com>
2026-03-31 10:51:09 +0530

    dynamic api

--- src/app.js
+++ src/app.js
@@ -3,25 +3,75 @@ const connectDB = require ("./config/database")
 const User = require("./models/user")
 const app = express();
 
-app.post("/signup",async(req,res)=>{
-    const userObj = {
-        firstName:"Kim",
-        lastName:"Taehyung",
-        emailId:"tae@gmail.com",
-        password:"Tae@1234"
-    }
-    const user = new User(userObj)
+app.use(express.json())
+
+app.post("/signup",async(req,res)=>{    
+    const user = new User(req.body)
     try{
          await user.save()
     res.send("user saved successfully")
     }
     catch{
         res.status(400).send("error saving the user:"+ err.message)
-    }
-   
+    }  
 })
 
+//Feed api - get all users from database
+app.get("/feed",async(req,res)=>{
+    try{
+    const users = await User.find({})
+    res.send(users)
+    }
+    catch(err){
+        res.status(400).send("something went wrong")
+    }
+})
 
+app.get("/user",async(req,res)=>{
+       const userEmail = req.query.emailId
+       console.log("Query object:", req.query);
+    try{
+   const userbyId = await User.findOne({emailId:userEmail})
+   if(!userbyId){
+
+   return res.status(400).send("user not found")
+   }
+   else{
+    return res.send(userbyId)
+   }
+//    if(userbyId.length===0){
+//          res.status(400).send("user not found")      
+//    }
+//    else{
+//    res.send(userbyId)
+//    }
+    }
+    catch(err){
+     res.status(400).send("something went wrong")      
+    }
+})
+
+app.patch("/user",async(req,res)=>{
+    const userId = req.body.userId;
+    const data = req.body
+    try{
+await User.findByIdAndUpdate(userId,data);
+res.send("user saved successfully")
+    }catch(err){
+     res.status(400).send("something went wrong")      
+    }
+})
+
+app.delete("/user",async(req,res)=>{
+const userId = req.body.userId;
+try{
+const user = await User.findByIdAndDelete(userId);
+res.send("User deleted successfully")
+}
+catch(err){
+     res.status(400).send("something went wrong")      
+    }
+})
 connectDB().then(()=>{
     console.log("connected to DB");
     app.listen(3000,()=>{
