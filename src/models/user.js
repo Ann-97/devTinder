928330fe3f7a4c79ceb10183aeb8fc778b0de1ef
Ann Maria Varghese <annmariavithayathil@gmail.com>
2026-03-31 10:51:33 +0530

    schema validations

--- src/models/user.js
+++ src/models/user.js
@@ -1,14 +1,47 @@
 const mongoose = require ("mongoose")
 
 const userSchema = new mongoose.Schema({
-    firstName : {type:String},
-    lastName : {type:String},
-    email : {type:String},
-    password : {type:String},
-    gender : {type:String},
-    age : {type:Number},
+    firstName : {
+        type:String,
+        required:true
+    },
+    lastName :
+     {
+        type:String
+    },
+    emailId : {
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
+    },
+    password : {
+        type:String,
+        required:true 
+      },
+    gender : {
+        type:String
+    },
+    age : {
+        type:Number,
+      validate(value){
+        if(!["male","female","others"].includes(value)){
+            throw new error("Gender not valid")
+        }
+      }
+    },
+     photoUrl : {
+        type:String,
+        default:"https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-2210.jpg?semt=ais_incoming&w=740&q=80"
+      },
+       about : {
+        type:String,
+        default: "This is a default about of the user"
+      },
+      
 
-})
+},{timestamps:true})
 
 const User = mongoose.model("user",userSchema)
 module.exports = User
